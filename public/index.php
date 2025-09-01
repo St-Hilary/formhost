cf15d6508412ba4590c4b4ef158e31d6b7f62ead
Robert Hohman <robert@glassdoor.com>
2025-09-01 12:47:23 -0700

    form library

--- public/index.php
+++ public/index.php
@@ -12,10 +12,17 @@
         <h1>Thank you for visiting St Hilary</h1>
         <h3>If you have need of a service from St Hilary, please feel free to submit one of the forms below. God Bless.</h3>
         <ul>
+            <li><a href="/form/?id=9C4754D8-D3CD-4239-ABE1-9D83C2AFDE4D">Contact Us</a></li>
             <li><a href="/form/?id=1E23CB89-9EB6-474E-B7F3-FB81A66DE1AE">Child Baptism Inquiry Form</a></li>
             <li><a href="/form/?id=0b3b8dc2-9212-4fa1-acc1-e5d9499485df">Child Baptism Sponsorship Certificate</a></li>
             <li><a href="/form/?id=5807a16d-dc85-4eb7-8e57-09c685e22e5d">Funeral Intake Form</a></li>
             <li><a href="/form/?id=1979f796-c351-4149-b7ab-68dd63207f6d">Stewardship Faith Commitment</a></li>
+            <li><a href="/form/?id=EB98D96C-B149-40A2-87DC-CD5248C9D61C">Duplicate Sacramental Records Request</a></li>
+            <li><a href="/form/?id=26BDDD9D-93E5-4D88-BA55-662171CA5E7E">Event Submission</a></li>
+            <li><a href="/form/?id=07E91831-0A3E-4F13-BF68-798C3DF690A8">Become a Member</a></li>
+            <li><a href="/form/?id=05382102-D18D-4DE1-8860-D85C63B2D9FA">Confession</a></li>
+            <li><a href="/form/?id=5AF63399-E526-4559-8356-C074D1315B60">Join a Ministry Team</a></li>
+            <li><a href="/form/?id=66C1B67A-EEAB-424C-AC91-0FCCC239CC6B">Join a Ministry Team</a></li>
         </ul>
     </div>
 </body>
