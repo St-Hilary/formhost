0209d68e90cea72ba11aa413dc34009392d5077b
Robert Hohman <robert@glassdoor.com>
2025-09-01 10:50:42 -0700

    home page styling

--- public/index.php
+++ public/index.php
@@ -3,15 +3,18 @@
 <head>
     <meta charset="utf-8">
     <title>St. Hilary Church - Making Jesus Matter to Everyone</title>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-<img src="/img/sthilary.png" width="128" height="128" />
-<h1>Forms Available:</h1>
-<ul>
-    <li><a href="/form/?id=1E23CB89-9EB6-474E-B7F3-FB81A66DE1AE">Child Baptism Inquiry Form</a></li>
-    <li><a href="/form/?id=0b3b8dc2-9212-4fa1-acc1-e5d9499485df">Child Baptism Sponsorship Certificate</a></li>
-    <li><a href="/form/?id=5807a16d-dc85-4eb7-8e57-09c685e22e5d">Funeral Intake Form</a></li>
-    <li><a href="/form/?id=1979f796-c351-4149-b7ab-68dd63207f6d">Stewardship Faith Commitment</a></li>
-</ul>
+    <img src="/img/sthilary.png" width="128" height="128" />
+    <div class="main-container">
+        <h1>Forms Available:</h1>
+        <ul>
+            <li><a href="/form/?id=1E23CB89-9EB6-474E-B7F3-FB81A66DE1AE">Child Baptism Inquiry Form</a></li>
+            <li><a href="/form/?id=0b3b8dc2-9212-4fa1-acc1-e5d9499485df">Child Baptism Sponsorship Certificate</a></li>
+            <li><a href="/form/?id=5807a16d-dc85-4eb7-8e57-09c685e22e5d">Funeral Intake Form</a></li>
+            <li><a href="/form/?id=1979f796-c351-4149-b7ab-68dd63207f6d">Stewardship Faith Commitment</a></li>
+        </ul>
+    </div>
 </body>
 </html>
