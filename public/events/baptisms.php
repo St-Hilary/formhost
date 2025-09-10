fc3c3a08d24b0cb8b103c13c1dc1621e7af818b8
Robert Hohman <robert@glassdoor.com>
2025-09-10 14:35:52 -0700

    events hosting

---
+++ public/events/baptisms.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>St. Hilary Church - Making Jesus Matter to Everyone</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans&display=swap' rel='stylesheet'>
+    <link rel="icon" type="image/ico" href="/img/favicon.ico">
+    <script id="MPWidgets" src="https://mp.sthilary.org/widgets/dist/MPWidgets.js"></script>
+</head>
+<body>
+<img style="padding-top: 30px; padding-left: 50px;" src="/img/sthilary.png" width="128" height="128" />
+<mpp-event-finder congregationid="1" eventtypeid="15"></mpp-event-finder>
+</body>
+</html>
