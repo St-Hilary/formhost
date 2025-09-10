c9ddf29fd2fa21cb2b5cd07cf64ed3e6e8bec309
Robert Hohman <robert@glassdoor.com>
2025-09-10 16:10:02 -0700

    events page

---
+++ public/events/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>St. Hilary Church - Making Jesus Matter to Everyone</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans&display=swap' rel='stylesheet'>
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="icon" type="image/ico" href="/img/favicon.ico">
+</head>
+<body>
+<div class="main-container">
+    <a href="https://sthilary.org"><img src="/img/sthilary.png" width="128" height="128" /></a>
+    <h1>Thank you for visiting St Hilary</h1>
+    <h3>If you have come looking for specific events, try one of these:</h3>
+    <ul>
+        <li><a href="/events/baptisms.php">Baptisms</a></li>
+        <li><a href="/events">All events</a></li>
+    </ul>
+</div>
+</body>
+</html>
