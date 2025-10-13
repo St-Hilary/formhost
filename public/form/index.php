6c0d441c0fc7dfa7ac16734235a89afa79a0e8f5
Robert Hohman <robert@glassdoor.com>
2025-10-13 11:45:19 -0700

    put custom css back

--- public/form/index.php
+++ public/form/index.php
@@ -3,11 +3,12 @@
   <head>
       <meta charset="utf-8">
       <title>St. Hilary Church - Making Jesus Matter to Everyone</title>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans&display=swap' rel='stylesheet'>
       <link rel="icon" type="image/ico" href="/img/favicon.ico">
       <script id="MPWidgets" src="https://mp.sthilary.org/widgets/dist/MPWidgets.js"></script>
       </head>
   <body>
       <img style="padding-top: 30px; padding-left: 50px;" src="/img/sthilary.png" width="128" height="128" />
-   <mpp-custom-form></mpp-custom-form>
+   <mpp-custom-form customcss="https://forms.sthilary.org/css/custom-widget.css"></mpp-custom-form>
    </body>
 </html>
