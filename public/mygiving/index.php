96455be1712a9dd42a7f6f6733b266b938a28004
Robert Hohman <robert@glassdoor.com>
2026-01-29 08:35:01 -0800

    giving

---
+++ public/mygiving/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>St. Hilary Church - Making Jesus Matter to Everyone</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans&display=swap' rel='stylesheet'>
+    <link rel="icon" type="image/ico" href="/img/favicon.ico">
+    <script id="MPWidgets" src="https://mp.sthilary.org/widgets/dist/MPWidgets.js"></script>
+</head>
+<body>
+<img style="padding-top: 30px; padding-left: 50px;" src="/img/sthilary.png" width="128" height="128" />
+<mpp-my-giving hidesoftcredits="false"></mpp-my-giving>
+
+</body>
+</html>
