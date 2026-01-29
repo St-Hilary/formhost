4fb434216b157bbd8f5ff518d931c8c2ba632b55
Robert Hohman <robert@glassdoor.com>
2026-01-29 08:48:43 -0800

    no caching

--- public/mygiving/index.php
+++ public/mygiving/index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate, max-age=0">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>St. Hilary Church - Making Jesus Matter to Everyone</title>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans&display=swap' rel='stylesheet'>
     <link rel="icon" type="image/ico" href="/img/favicon.ico">
