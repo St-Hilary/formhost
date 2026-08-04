61a546cdeee15bab35972c375f0e36f3827e1ac0
bobhohman <robert@robertandfrannie.com>
2026-08-04 13:25:51 -0700

    fix signup

--- public/groups/details.php
+++ public/groups/details.php
@@ -11,7 +11,47 @@
 <body>
 <img style="padding-top: 30px; padding-left: 50px;" src="/img/sthilary.png" width="128" height="128" />
 
-<mpp-group-details returnurl="/groups"></mpp-group-details>
+<mpp-group-details returnurl="/groups" hidesignuptab="true"></mpp-group-details>
+
+<script>
+    // Rename the "Contact this Group" tab/form to "Sign up for this group".
+    // The widget renders into an open shadow root, so we reach in and swap the
+    // label's text node once the widget has rendered.
+    (function () {
+        var NEW_LABEL = 'Sign up for this group';
+
+        function renameTab(root) {
+            var tab = root.querySelector('#inquireTab');
+            if (!tab) return false;
+            for (var i = 0; i < tab.childNodes.length; i++) {
+                var node = tab.childNodes[i];
+                if (node.nodeType === Node.TEXT_NODE && node.textContent.trim()) {
+                    if (node.textContent.trim() !== NEW_LABEL) node.textContent = NEW_LABEL;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function start() {
+            var widget = document.querySelector('mpp-group-details');
+            if (!widget || !widget.shadowRoot) return false;
+            var root = widget.shadowRoot;
+            if (!renameTab(root)) return false;
+            // Re-apply if the widget re-renders (e.g. after an attribute change).
+            new MutationObserver(function () { renameTab(root); })
+                .observe(root, { childList: true, subtree: true });
+            return true;
+        }
+
+        if (!start()) {
+            var tries = 0;
+            var timer = setInterval(function () {
+                if (start() || ++tries > 200) clearInterval(timer);
+            }, 100);
+        }
+    })();
+</script>
 
 </body>
 </html>
